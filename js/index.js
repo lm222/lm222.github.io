2936c680020d3a77c5146e0fe57ba5f851c46b5f
lm222 <leah@dendritecorp.com>
2015-12-03 16:55:18 -0500

    indentation

--- js/index.js
+++ js/index.js
@@ -2,88 +2,89 @@ $(document).ready(function(){
 
 // slider
 
-      images = ['images/hand1.png', 'images/hand2.png', 'images/hand3.png'];
+  images = ['images/hand1.png', 'images/hand2.png', 'images/hand3.png'];
 
+  beginNow = setInterval(forwardImage, 4000);
 
-      beginNow = setInterval(forwardImage, 4000);
+    function currentImageKey() {
+        i = jQuery.inArray($('#slideshow').attr('src'), images);
+        return i;
+    }
 
+    function forwardImage() {
+        currentImageKey();
+        if (i < images.length - 1) {
+          changeImage(i + 1);
+        } else {
+          changeImage(0);
+        }
+    }
 
-      function currentImageKey() {
-          i = jQuery.inArray($('#slideshow').attr('src'), images);
-          return i;
-      }
-
-
-      function forwardImage() {
-          currentImageKey();
-          if (i < images.length - 1) {
-              changeImage(i + 1);
-          } else {
-              changeImage(0);
-          }
-      }
-
-      function changeImage(i) {
-          $('#slideshow').stop().animate({
-              opacity: 0,
-          }, 200, function() {
-              $('#slideshow').attr('src', images[i]);
-              $('#holder img').load(function() {
-                  $('#slideshow').stop().animate({
-                      opacity: 1,
-                  }, 200)
-              })
+    function changeImage(i) {
+        $('#slideshow').stop().animate({
+          opacity: 0,
+        }, 
+        200, function() {
+            $('#slideshow').attr('src', images[i]);
+            $('#holder img').load(function() {
+              $('#slideshow').stop().animate({
+                  opacity: 1,
+              }, 200)
           })
-      }
+        })
+    }
 
-      $('#left-a').click(function() {
-          clearInterval(beginNow);
-          backwardsImage();
-      });
+    //arrows
 
-      $('#right-a').click(function() {
+    $('#left-a').click(function() {
+        clearInterval(beginNow);
+        backwardsImage();
+    });
+
+    $('#right-a').click(function() {
         clearInterval(beginNow);
         forwardImage();
-      });
+    });
 
 
-      function backwardsImage() {
-      currentImageKey();
-      if (i == 0) {
-        changeImage(images.length - 1);
-      } else {
-        changeImage(i - 1);
-      }
-      };
+    function backwardsImage() {
+    currentImageKey();
+    if (i == 0) {
+    changeImage(images.length - 1);
+    } else {
+    changeImage(i - 1);
+    }
+    };
 
-      //animate gif on hover
+    //animate gif on hover
 
-      $(function(){
+    $(function(){
         $('.img_wrap').hover(function() {
-         $(this).toggleClass('animated', 'static');
-      })
-      });
+            $(this).toggleClass('animated', 'static');
+        })
+    });
 
 
-      //random word
+    //random word
 
-      // $(function(){
-      $('#random-word').text("inspiring!");
+    // $(function(){
+    $('#random-word').text("inspiring!");
 
-      $('#word').click(function(){
-      $.ajax({
+    $('#word').click(function(){
+        $.ajax({
         url: 'http://randomword.setgetgo.com/get.php',
         method: 'GET',
         jsonp: 'callback',
         dataType: 'jsonp',
-          success: function(data){
+            success: function(data){
             $('#random-word').text(data["Word"] + '!');
-          }
+            }
         })
-      });
-      $('#word').click(function(){
+    });
+
+    $('#word').click(function(){
         $(".girl").toggleClass('boy', 'girl');
-      })
+    })
 
 });
 
